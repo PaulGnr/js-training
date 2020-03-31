ad8f178b33f44bf2be34069bba23c05526f72c6a
Christopher Fremond <christopher.fremond@gmail.com>
2020-03-31 19:12:15 +0100

    working solutions

--- tests/cut-corners/pass_frenchris.js
+++ tests/cut-corners/pass_frenchris.js
@@ -1,1 +1,7 @@
-const round = el => (el % 10 > 5 ? (el + 1) / 1 : el / 1)
+const trunc = el => (el >= 0 ? el - (el % 1) : el - (el % 1))
+
+const round = el =>
+  el % 1 >= 0.5 || el % 1 <= -0.5 ? trunc(el >= 0 ? el + 1 : el - 1) : trunc(el)
+
+const ceil = el => (el % 1 > 0 ? trunc(el >= 0 ? el + 1 : el - 1) : trunc(el))
+const floor = el => (el % 1 < 0 ? trunc(el >= 0 ? el + 1 : el - 1) : trunc(el))
