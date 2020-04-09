d5913419fe89fcfdb06787aea06c688967b953d8
Christopher Fremond <christopher.fremond@gmail.com>
2020-04-09 15:21:46 +0100

    correct solution for cut-cornes ex

--- tests/cut-corners/pass_frenchris.js
+++ tests/cut-corners/pass_frenchris.js
@@ -1,4 +1,4 @@
-const trunc = el => (el >= 0 ? el - (el % 1) : el - (el % 1))
+const trunc = el => el - (el % 1)
 
 const round = el =>
   el % 1 >= 0.5 || el % 1 <= -0.5 ? trunc(el >= 0 ? el + 1 : el - 1) : trunc(el)
